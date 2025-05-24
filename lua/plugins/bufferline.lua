b8b8d3f40c79c69a59ba440f494c3b5df0c70d95
iagorrr <iagorrr04@gmail.com>
2025-05-24 10:55:58 -0300

    add bufferline which-key bidns

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -38,8 +38,18 @@ return {
 
     mappings = {
         n = {
-            ["<S-h>"] = { "<cmd>BufferLineCyclePrev<cr>", "Go to Previous Buffer" },
-            ["<S-l>"] = { "<cmd>BufferLineCycleNext<cr>", "Go to Next Buffer" },
+            ["<leader>b"] = {
+                name = "Buffers",
+                j = { "<cmd>BufferLinePick<cr>", "Jump" },
+                f = { "<cmd>Telescope buffers previewer=false<cr>", "Find" },
+                h = { "<cmd>BufferLineCyclePrev<cr>", "Previous" },
+                l = { "<cmd>BufferLineCycleNext<cr>", "Next" },
+                H = { "<cmd>BufferLineCloseLeft<cr>", "Close all to the left" },
+                L = {
+                    "<cmd>BufferLineCloseRight<cr>",
+                    "Close all to the right",
+                },
+            },
         },
     },
 }
