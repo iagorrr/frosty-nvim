d153eebbda04b5d3799129cf8f140103bf235b43
iagorrr <iagorrr04@gmail.com>
2025-08-17 22:07:10 -0300

    feat: add noice plugin with better ui for cmdline

--- flake.nix
+++ flake.nix
@@ -224,6 +224,11 @@
       url = "github:folke/which-key.nvim";
       flake = false;
     };
+
+    "noice.nvim" = {
+      url = "github:folke/noice.nvim";
+      flake = false;
+    };
   };
   # PLUGINS END
 }
