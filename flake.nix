0cb59946c84db58cb25c44ec2f55e9821664119f
iagorrr <iagorrr04@gmail.com>
2025-09-06 10:59:45 -0300

    feat: add spell checker for both 'en' and 'pt-br'
    Enables the builtin Neovim spell checker for english and
    Brazilian portuguese.

--- flake.nix
+++ flake.nix
@@ -5,7 +5,6 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-
   outputs = {
     self,
     nixpkgs,
@@ -168,6 +167,10 @@
       url = "github:NeogitOrg/neogit";
       flake = false;
     };
+    "noice.nvim" = {
+      url = "github:folke/noice.nvim";
+      flake = false;
+    };
     "nui.nvim" = {
       url = "github:MunifTanjim/nui.nvim";
       flake = false;
@@ -222,11 +225,6 @@
     };
     "which-key.nvim" = {
       url = "github:folke/which-key.nvim";
-      flake = false;
-    };
-
-    "noice.nvim" = {
-      url = "github:folke/noice.nvim";
       flake = false;
     };
   };
