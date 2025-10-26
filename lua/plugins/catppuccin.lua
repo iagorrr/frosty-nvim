cd866b55d389b7a07997a6da50e559394f91ff21
iagorrr <iagorrr04@gmail.com>
2025-10-26 16:09:17 -0300

    ui: set background transparency and set catppuccin flavor to frappe

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,39 +1,20 @@
 local default_opts = {
-    flavour = "mocha",
+    flavour = "frappe",
 
-    -- TODO Make a prettier colorscheme (https://catbbrew.com/)
-    color_overrides = {
-        mocha = {
-            rosewater = "#f5e0dc",
-            flamingo = "#f2cdcd",
-            pink = "#f5c2e7",
-            mauve = "#cba6f7",
-            red = "#f38ba8",
-            maroon = "#eba0ac",
-            peach = "#fab387",
-            yellow = "#f9e2af",
-            green = "#a6e3a1",
-            teal = "#94e2d5",
-            sky = "#89dceb",
-            sapphire = "#74c7ec",
-            blue = "#89b4fa",
-            lavender = "#b4befe",
-            text = "#cbd7f4",
-            subtext1 = "#b8c3de",
-            subtext0 = "#a3aec8",
-            overlay2 = "#909ab2",
-            overlay1 = "#7c859c",
-            overlay0 = "#697186",
-            surface2 = "#545c70",
-            surface1 = "#41485a",
-            surface0 = "#2d3344",
-            base = "#1a1f2e",
-            mantle = "#151925",
-            crust = "#0e121b",
-        },
+    background = {
+        light = "latte",
+        dark = "frappe",
     },
 
-    default_integrations = false,
+    transparent_background = true,
+
+    float = {
+        transparent = true, -- enable transparent floating windows
+        solid = false, -- use solid styling for floating windows, see |winborder|
+    },
+
+    default_integrations = true,
+
     integrations = {
         markdown = true,
         semantic_tokens = true,
@@ -63,10 +44,10 @@ local function config(_, opts)
     vim.cmd.colorscheme "catppuccin"
 end
 
-local function bufferline_opts(_, opts)
-    opts.highlights = require("catppuccin.groups.integrations.bufferline").get_theme()
-end
-
+-- local function bufferline_opts(_, opts)
+--     opts.highlights = require("catppuccin.groups.integrations.bufferline").get_theme()
+-- end
+--
 return {
     "catppuccin/nvim",
     name = "catppuccin",
@@ -77,9 +58,9 @@ return {
     opts = default_opts,
     config = config,
 
-    specs = {
-        "akinsho/bufferline.nvim",
-        optional = true,
-        opts = bufferline_opts,
-    },
+    -- specs = {
+    --     "akinsho/bufferline.nvim",
+    --     optional = true,
+    --     opts = bufferline_opts,
+    -- },
 }
