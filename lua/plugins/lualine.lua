dc4fce894f8a7e51b235ecfbf877826b40963f21
iagorrr <iagorrr04@gmail.com>
2025-11-15 09:17:37 -0300

    ui: make lualine disabled by default and create a bind for toggle

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,11 +2,29 @@
 -- TODO: Lazy load lualine
 -- TODO: Catppuccin integration
 
+function ToggleLualine()
+    if vim.opt.laststatus:get() > 0 then
+        require("lualine").hide()
+        vim.opt.laststatus = 0
+    else
+        require("lualine").hide { unhide = true }
+    end
+end
+
+local function config()
+    require("lualine").setup()
+    require("lualine").hide()
+    vim.opt.laststatus = 0
+end
 return {
     "nvim-lualine/lualine.nvim",
     dependencies = "nvim-tree/nvim-web-devicons",
 
     lazy = false,
 
-    opts = {},
+    config = config,
+
+    keys = {
+        { "<leader>uL", ToggleLualine, "Toggle lualine" },
+    },
 }
