4c8bf7ba53e69aef93508bb81a64e86ec6096804
iagorrr <iagorrr04@gmail.com>
2026-06-02 01:43:54 -0300

    Remove line indicator when entering Zen Mode

--- lua/plugins/snacks/zen.lua
+++ lua/plugins/snacks/zen.lua
@@ -2,6 +2,29 @@ return {
     opts = {
         toggles = {
             dim = false,
+            number = false,
+            relativenumber = false,
         },
+        center = true,
+        show = {
+            statusline = false,
+        },
+        win = { style = "zen" },
+
+        zoom = {
+            show = {
+                statusline = false,
+                tabline = false,
+            },
+        },
+
+        on_open = function()
+            vim.opt.number = false
+            vim.opt.relativenumber = false
+        end,
+        on_close = function()
+            vim.opt.number = true
+            vim.opt.relativenumber = true
+        end,
     },
 }
